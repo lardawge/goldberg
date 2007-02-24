7edbc5b749c11f6133f155eb276c438c30a5acba
Dave Nelson <urbanus@240gl.org>
2007-02-24 00:52:26 +0000

    FILE REORGANISATION: Fixed various views, the default layout and stylesheets.  Added auto-detection for multiple controller paths.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/goldberg/trunk/goldberg/templates/vendor/plugins/goldberg@72 2112df0d-361b-0410-855b-e9e795e2bf81

--- app/models/goldberg/roles_permission.rb
+++ app/models/goldberg/roles_permission.rb
@@ -3,12 +3,13 @@ module Goldberg
     include GoldbergModel
     
     def RolesPermission.find_for_role(role_ids)
-      return find_by_sql([%q{
+      querystr = <<-END
 select rp.*, p.name 
 from #{prefix}roles_permissions rp inner join #{prefix}permissions p 
   on rp.permission_id = p.id 
 where role_id in (?) order by p.name
-}, role_ids])
+END
+      return find_by_sql([querystr, role_ids])
     end
     
   end
--- app/models/goldberg/site_controller.rb
+++ app/models/goldberg/site_controller.rb
@@ -10,36 +10,54 @@ module Goldberg
       @permission ||= Permission.find_by_id(self.permission_id)
       return @permission
     end
-
+    
     def actions
       @actions ||= ControllerAction.find(:all,
                                          :conditions =>
                                          "site_controller_id = #{self.id}",
                                          :order => 'name')
     end
-
-
+    
     def self.classes
-      for file in Dir.glob("#{RAILS_ROOT}/app/controllers/*.rb") do
-        begin
-          load file
-        rescue
-          logger.info "Couldn't load file '#{file}' (already loaded?)"
-        end
-      end
+      #     for file in Dir.glob("#{RAILS_ROOT}/app/controllers/*.rb") do
+      #       begin
+      #         load file
+#       rescue
+      #         logger.info "Couldn't load file '#{file}' (already loaded?)"
+      #       end
+      #     end
       
+      for path in ActionController::Routing.controller_paths do
+        self.load_class_files(path)
+      end  
+
       classes = Hash.new
       
       ObjectSpace.each_object(Class) do |klass|
-        if klass.respond_to? :controller_name
+        if klass.respond_to? :controller_path
           if klass.superclass.to_s == ApplicationController.to_s
-            classes[klass.controller_name] = klass
+            classes[klass.controller_path] = klass
           end
         end
       end
-
+      
       return classes
     end
-
+    
+    
+    def self.load_class_files(path)
+      for file in Dir.glob("#{path}/*") do
+        if file.match /\.rb$/
+          begin
+            load file
+          rescue
+            logger.info "Couldn't load file '#{file}' (already loaded?)"
+          end
+        elsif File.directory? file
+          self.load_class_files(file)
+        end
+      end
+    end
+    
   end
 end
