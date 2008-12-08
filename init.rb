542ea84188766eb87db3c24b8eab2c898b0644bf
Larry Sprock <larry@lucidbleu.com>
2008-12-08 14:42:59 -0500

    added to init.rb file for 2.2 compatibility.

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ model_path      = "#{File.dirname(__FILE__)}/app/models"
 helper_path     = "#{File.dirname(__FILE__)}/app/helpers"
 $LOAD_PATH << controller_path
 $LOAD_PATH << model_path
-Dependencies.load_paths += [ controller_path, model_path, helper_path ]
+ActiveSupport::Dependencies.load_paths += [ controller_path, model_path, helper_path ]
 config.controller_paths << controller_path
 
 # Goldberg's libraries
